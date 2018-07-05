7a0f763c7db1609e728c9d2ab563aaa4d8ad59a4
sirdavidoff <davewroberts@yahoo.com>
2018-07-05 20:11:21 +0200

    Alfred workflow for managing DNS not uses a script filter

---
+++ Alfred/Alfred.alfredpreferences/workflows/user.workflow.411AFB42-3CE9-4B76-849A-5994EA61B1FB/dns_options.py
@@ -0,0 +1,57 @@
+import sys
+import subprocess
+from workflow import Workflow
+
+commands = [
+                {
+                    "title":"No special DNS",
+                    "subtitle":"",
+                    "check":"There aren't any DNS Servers set on Wi-Fi.",
+                    "arg":"Empty"
+                },
+                {
+                    "title":"Getflix Frankfurt DNS",
+                    "subtitle":"54.93.169.181",
+                    "check":"54.93.169.181",
+                    "arg":"54.93.169.181"
+                },
+                {
+                    "title":"Getflix Copenhagen DNS",
+                    "subtitle":"82.103.129.240",
+                    "check":"82.103.129.240",
+                    "arg":"82.103.129.240"
+                }
+            ]
+
+
+def main(wf):
+
+    global commands
+
+    # Work out what DNS is set at the moment and add it as the first result
+    current = subprocess.check_output(["networksetup", "-getdnsservers", "Wi-Fi"]).strip()
+    matching_commands = [x for x in commands if x['check'] == current]
+    current_cmd = None
+    if len(matching_commands) > 0:
+        current_cmd = matching_commands[0]
+        wf.add_item(title=current_cmd['title'],
+                    subtitle=current_cmd['subtitle'],
+                    arg=current_cmd['arg'],
+                    icon="icon_selected.png",
+                    valid=True)
+
+    # Add the rest of the results
+    for cmd in commands:
+        if cmd != current_cmd:
+            wf.add_item(title=cmd['title'],
+                        subtitle=cmd['subtitle'],
+                        arg=cmd['arg'],
+                        valid=True)
+
+    # Send the results to Alfred as XML
+    wf.send_feedback()
+
+
+if __name__ == u"__main__":
+    wf = Workflow()
+    sys.exit(wf.run(main))
